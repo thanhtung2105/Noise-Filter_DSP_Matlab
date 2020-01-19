c8b8745762b7bb1428b585970a606ffb4f62cf75
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-01-19 22:23:02 +0700

    Create BT31.m

---
+++ My studying/Practice/BT31.m
@@ -0,0 +1,10 @@
+rp = 0.5;
+rs = 40;
+fs = 4000;
+f = [800 1000];
+a = [1 0];
+dev = [(10^(rp/20)-1)/(10^(rp/20)+1) 10^(-rs/20)];
+[n,fo,ao,w] = firpmord(f,a,dev,fs);
+b = firpm(n,fo,ao,w);
+freqz(b,1,1024,fs);
+title('FIR');
